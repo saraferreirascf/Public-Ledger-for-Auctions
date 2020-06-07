48139f71972ebc502c6d5d4215dd8f5fef146671
sariscas <saraferreira.p3@gmail.com>
2020-06-07 23:17:18 +0100

    Added menus

--- src/main/java/Kademlia.java
+++ src/main/java/Kademlia.java
@@ -40,6 +40,7 @@ public class Kademlia {
             @Override
             public void run()
             { 
+                Menu_Inicial();
                 try { 
                     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
                     String line = "";
@@ -49,7 +50,7 @@ public class Kademlia {
 
                         String[] commands = line.split(" ");
 
-                        if (commands[0].equals("createhardcodednode")) {
+                        if (commands[0].equals("CreateHardCodedNode")) {
                             logger.info("Selected createhardcodednode");
                             tree = new Binary_tree(50051);
                             Thread tserver = new Thread(new Runnable() { 
@@ -69,7 +70,7 @@ public class Kademlia {
                             logger.info("Created master node");
                             tree.chain.printChain();
                                 
-                        } else if (commands[0].equals("createminer")) {
+                        } else if (commands[0].equals("CreateMiner")) {
                             logger.info("Selected creatminer");
                             tree = new Binary_tree();
                             Thread tserver = new Thread(new Runnable() { 
@@ -110,7 +111,7 @@ public class Kademlia {
                             tree.inserts(tree.current);
                             tree.testUnit.insertNodes();
                             tree.testUnit.printallnodes();
-                        }  else if (commands[0].equals("createuser")) {
+                        }  else if (commands[0].equals("CreateUser")) {
                             tree = new Binary_tree(commands[1]);
                             System.out.println();
                             System.out.println("Welcome to the network "+commands[1]);
@@ -128,27 +129,29 @@ public class Kademlia {
                             tserver.start();
                             // join
                             tree.inserts(tree.current);
-                            logger.info("Created node");
+                            //logger.info("Created node");
                             // este nó conhece o hardcoded 127.0.0.1:50051
                             Binary_tree.Master_node master = tree.new Master_node(50051);
                             tree.inserts(master);
-                            logger.info("Created master node");
-                            System.out.println("Tamanho closest: " + tree.lookup(tree.current.nodeID).size());
+                            //logger.info("Created master node");
+                            //System.out.println("Tamanho closest: " + tree.lookup(tree.current.nodeID).size());
 
                            // tree.chain.printChain(); // ainda esta vazia pq nao pediu nada ao master
                             tree.updateBlockchain(master);
                             tree.getMinersFromMaster(master);
 
                             System.out.println("Waiting for transactions...");
+
+                           // Menu();
                             
 
                             
-                        } else if (commands[0].equals("printallnodes")) {
+                        } else if (commands[0].equals("PrintAllNodes")) {
                             
                             tree.testUnit.printallnodes();
-                        } else if (commands[0].equals("printallchain")) {
+                        } else if (commands[0].equals("PrintAllChain")) {
                             tree.chain.printChain();
-                        } else if (commands[0].equals("sendtransaction")){
+                        } else if (commands[0].equals("SendTransaction")){
                             System.out.println();
                             System.out.println("Please indicate the recipient of the transation");
 
@@ -198,6 +201,51 @@ public class Kademlia {
         });
         cmdlistener.start();
     }
+
+
+    public static void Menu_Inicial(){
+        System.out.println("************************************");
+        System.out.println("* Welcome to our Public Ledger! :) *");
+        System.out.println("************************************");
+        System.out.println();
+        System.out.println();
+        System.out.println("-------------------");
+        System.out.println("Available commands:");
+        System.out.println("-------------------");
+        System.out.println();
+        System.out.println("-CreateHardCodedNode");
+        System.out.println("-CreateMiner");
+        System.out.println("-CreateUser <username>");
+        System.out.println("-SendTransaction");
+        System.out.println("-PrintAllNodes");
+        System.out.println("-PrintAllChain");
+        System.out.println("-quit");
+        System.out.println();
+        System.out.println();
+        System.out.print("Option: ");
+
+ }
+
+ public static void Menu(){
+    System.out.println("************************************");
+    System.out.println("*              USER MENU           *");
+    System.out.println("************************************");
+    System.out.println();
+    System.out.println();
+    System.out.println("-------------------");
+    System.out.println("Available commands:");
+    System.out.println("-------------------");
+    System.out.println();
+    System.out.println("-SendTransaction");
+    System.out.println("-PrintAllNodes");
+    System.out.println("-PrintAllChain");
+    System.out.println("-quit");
+    System.out.println();
+    System.out.println();
+    System.out.print("Option: ");
+
+}
+
 
     public static void main(String[] args) {
 
